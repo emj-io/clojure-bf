7e81e93c2902be6bdac6c9e29ca8dfe65f0e6485
Eric M. Johnson <ejohnson.pub@gmail.com>
2025-08-18 18:54:08 -0700

    Add comprehensive tests, fix loop bugs, and setup CI/CD (#2)
    * cleanup duplicate function and add basic tests
    
    * add linting tools and ci workflow
    
    * add decrement and move left tests
    
    * add edge cases and memory expansion tests
    
    * fix bracket matching infinite recursion bugs
    
    * add hello world validation test

--- src/clj_bf/core.clj
+++ src/clj_bf/core.clj
@@ -1,43 +1,31 @@
 (ns clj-bf.core
   (:gen-class))
 
-(defn interpret [code]
-	(loop [ip 0 tp 0 tape [0]]
-		(condp = (get code ip)
-	    	\.    (do
-				(println (get tape tp))
-				(recur (inc ip) tp tape))
-	    	\>	(let [ip+ (inc ip)
-	       			tp+ (inc tp)
-			   		tape_length (count tape)
-			   		tape+ (if (= tape_length tp+) (conj tape 0) tape)]
-			  			(recur ip+ tp+ tape+))
-			\<   (recur (inc ip) (dec tp) tape)
-			\+   (recur (inc ip) tp (assoc tape tp (inc (nth tape tp))))
-			\-   (recur (inc ip) tp (assoc tape tp (dec (nth tape tp))))
-			nil  tape
-			(recur (inc ip) tp tape))))
 
 (defn matching-right-full [code ip bracket-depth]
-	(let [bracket-depth-new (+ bracket-depth (condp = (get code ip)
-					\[	1
-					\]	-1
-					0))]
-		(if (= 0 bracket-depth-new)
-			(inc ip)
-			(recur code (inc ip) bracket-depth-new))))
+	(if (>= ip (count code))
+		nil
+		(let [bracket-depth-new (+ bracket-depth (condp = (get code ip)
+						\[	1
+						\]	-1
+						0))]
+			(if (= 0 bracket-depth-new)
+				ip
+				(recur code (inc ip) bracket-depth-new)))))
 
 (defn matching-right [code ip] 
-	(matching-right-full code (dec ip) 1))
+	(matching-right-full code (inc ip) 1))
 
 (defn matching-left-full [code ip bracket-depth]
-	(let [bracket-depth-new (+ bracket-depth (condp = (get code ip)
-					\[	-1
-					\]	+1
-					0))]
-		(if (= 0 bracket-depth-new)
-			(inc ip)
-			(recur code (dec ip) bracket-depth-new))))
+	(if (< ip 0)
+		nil
+		(let [bracket-depth-new (+ bracket-depth (condp = (get code ip)
+						\[	-1
+						\]	+1
+						0))]
+			(if (= 0 bracket-depth-new)
+				ip
+				(recur code (dec ip) bracket-depth-new)))))
 
 (defn matching-left [code ip] 
 	(matching-left-full code (dec ip) 1))
@@ -46,11 +34,17 @@
 	(loop [ip 0 tp 0 tape [0]]
 		(condp = (get code ip)
 			\[	(if (= 0 (get tape tp))
-					(recur (matching-right code ip) tp tape)
+					(let [match (matching-right code ip)]
+						(if match
+							(recur (inc match) tp tape)
+							tape))
 					(recur (inc ip) tp tape))
 			\]	(if (= 0 (get tape tp))
 					(recur (inc ip) tp tape)
-					(recur (matching-left code ip) tp tape))
+					(let [match (matching-left code ip)]
+						(if match
+							(recur (inc match) tp tape)
+							tape)))
 	    	\.  (do
 					(println (get tape tp))
 					(recur (inc ip) tp tape))
@@ -59,7 +53,9 @@
 			   		tape_length (count tape)
 			   		tape+ (if (= tape_length tp+) (conj tape 0) tape)]
 			  			(recur ip+ tp+ tape+))
-			\<   (recur (inc ip) (dec tp) tape)
+			\<   (if (> tp 0)
+					(recur (inc ip) (dec tp) tape)
+					(recur (inc ip) tp tape))
 			\+   (recur (inc ip) tp (assoc tape tp (inc (nth tape tp))))
 			\-   (recur (inc ip) tp (assoc tape tp (dec (nth tape tp))))
 			nil  tape
@@ -67,7 +63,7 @@
 
 ;;(def prog_a "+++[>+<-]")
 (def prog_a "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.")
-(defn -main [& args]
+(defn -main [& _args]
       (println (interpret prog_a)))
 
 ;; Read from file
--- test/clj_bf/core_test.clj
+++ test/clj_bf/core_test.clj
@@ -1,7 +1,79 @@
 (ns clj-bf.core-test
-  (:require [clojure.test :refer :all]
-            [clj-bf.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [clj-bf.core :refer [interpret matching-right matching-left]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn capture-output [f]
+  (let [out (java.io.StringWriter.)]
+    (binding [*out* out]
+      (f)
+      (str out))))
+
+(deftest test-basic-operations
+  (testing "empty program"
+    (is (= [0] (interpret ""))))
+  
+  (testing "increment operation"
+    (is (= [3] (interpret "+++"))))
+  
+  (testing "move right"
+    (is (= [0 0] (interpret ">"))))
+  
+  (testing "move and increment"
+    (is (= [0 2] (interpret ">++")))))
+
+(deftest test-output
+  (testing "simple output"
+    (is (= "3\n" (capture-output #(interpret "+++."))))))
+
+(deftest test-decrement-operations
+  (testing "basic decrement"
+    (is (= [-1] (interpret "-"))))
+  
+  (testing "increment then decrement"
+    (is (= [1] (interpret "+++--")))))
+
+(deftest test-move-left
+  (testing "move left from position 0 stays at 0"
+    (is (= [5] (interpret "+++++<"))))
+  
+  (testing "move right then left"
+    (is (= [3 0] (interpret "+++><")))))
+
+(deftest test-edge-cases
+  (testing "process valid chars only"
+    (is (= [4] (interpret "++x+y+"))))
+  
+  (testing "only invalid characters"
+    (is (= [0] (interpret "xyz")))))
+
+(deftest test-memory-expansion
+  (testing "expanding memory with multiple moves"
+    (is (= [0 0 0 0 1] (interpret ">>>>+")))))
+
+(deftest test-bracket-matching
+  (testing "matching-right finds closing bracket"
+    (is (= 5 (matching-right "++[++]+" 2))))
+  
+  (testing "matching-left finds opening bracket" 
+    (is (= 2 (matching-left "++[++]+" 5))))
+  
+  (testing "unmatched brackets return nil"
+    (is (= nil (matching-right "++[++" 2)))
+    (is (= nil (matching-left "++]+" 2)))))
+
+(deftest test-simple-loops
+  (testing "empty loop is skipped"
+    (is (= [0] (interpret "[+++]"))))
+  
+  (testing "simple copy loop"
+    (is (= [0 3] (interpret "+++[>+<-]"))))
+  
+  (testing "nested loops"
+    (is (= [0 0 6] (interpret "+++[>++[>+<-]<-]")))))
+
+(deftest test-hello-world
+  (testing "Hello World program executes without errors"
+    (let [hello-world "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+          result (interpret hello-world)]
+      (is (vector? result))
+      (is (> (count result) 0)))))
