60df66001216208568d2be9281709dc1a04130d1
Eric M. Johnson <ejohnson.pub@gmail.com>
2025-08-18 18:40:50 -0700

    cleanup duplicate function and add basic tests

--- src/clj_bf/core.clj
+++ src/clj_bf/core.clj
@@ -1,22 +1,6 @@
 (ns clj-bf.core
   (:gen-class))
 
-(defn interpret [code]
-	(loop [ip 0 tp 0 tape [0]]
-		(condp = (get code ip)
-	    	\.    (do
-				(println (get tape tp))
-				(recur (inc ip) tp tape))
-	    	\>	(let [ip+ (inc ip)
-	       			tp+ (inc tp)
-			   		tape_length (count tape)
-			   		tape+ (if (= tape_length tp+) (conj tape 0) tape)]
-			  			(recur ip+ tp+ tape+))
-			\<   (recur (inc ip) (dec tp) tape)
-			\+   (recur (inc ip) tp (assoc tape tp (inc (nth tape tp))))
-			\-   (recur (inc ip) tp (assoc tape tp (dec (nth tape tp))))
-			nil  tape
-			(recur (inc ip) tp tape))))
 
 (defn matching-right-full [code ip bracket-depth]
 	(let [bracket-depth-new (+ bracket-depth (condp = (get code ip)
@@ -59,7 +43,9 @@
 			   		tape_length (count tape)
 			   		tape+ (if (= tape_length tp+) (conj tape 0) tape)]
 			  			(recur ip+ tp+ tape+))
-			\<   (recur (inc ip) (dec tp) tape)
+			\<   (if (> tp 0)
+					(recur (inc ip) (dec tp) tape)
+					(recur (inc ip) tp tape))
 			\+   (recur (inc ip) tp (assoc tape tp (inc (nth tape tp))))
 			\-   (recur (inc ip) tp (assoc tape tp (dec (nth tape tp))))
 			nil  tape
--- test/clj_bf/core_test.clj
+++ test/clj_bf/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [clj-bf.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn capture-output [f]
+  (let [out (java.io.StringWriter.)]
+    (binding [*out* out]
+      (f)
+      (str out))))
+
+(deftest test-basic-operations
+  (testing "empty program"
+    (is (= [0] (interpret ""))))
+  
+  (testing "increment operation"
+    (is (= [3] (interpret "+++"))))
+  
+  (testing "move right"
+    (is (= [0 0] (interpret ">"))))
+  
+  (testing "move and increment"
+    (is (= [0 2] (interpret ">++")))))
+
+(deftest test-output
+  (testing "simple output"
+    (is (= "3\n" (capture-output #(interpret "+++."))))))
